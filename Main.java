5b98c6eb977b7aa987e367c509136cb811d1e275
tizzytzy <Sama.tzy@outlook.com>
2018-04-21 19:06:41 +0800

    修改逻辑语句

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -14,7 +15,7 @@ public class Main {
         weather.setTemperature(30);
         weather.setRain(false);
         weather.setUmbrella(false);
-        calendar.set(2018,4,20);
+        calendar.set(2018,3,20);
         weather.setWeek(calendar.getTime());
 
         calendar.clear();
@@ -23,7 +24,7 @@ public class Main {
         weather1.setTemperature(15);
         weather1.setRain(true);
         weather1.setUmbrella(true);
-        calendar.set(2018,4,21);
+        calendar.set(2018,3,21);
         weather1.setWeek(calendar.getTime());
 
         calendar.clear();
@@ -31,11 +32,28 @@ public class Main {
         list.add(weather);
         list.add(weather1);
 
-        System.out.println("最近" + list.size() + "天天气情况如下：");
-        System.out.println("日期 | 是否有雨 | 是否需要带伞 | 温度 | 着装建议 ");
-        for(Weather weather2 : list){
-            System.out.println(weather2.getWeek() + "|" + weather2.isRain() + "|" + weather2.isUmbrella() + "|" + weather2.getTemperature() + "|" + weather2.getClothesType());
-        }
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyy-mm-dd");
 
+        System.out.println("最近" + list.size() + "天天气情况如下：");
+        System.out.println("日期" + " |" + "温度");
+
+        System.out.println(simpleDateFormat.format(weather.getWeek()) +" |" + weather.getTemperature());
+        System.out.println(simpleDateFormat.format(weather1.getWeek()) + " |" + weather1.getTemperature());
+
+
+        for (Weather weather2 : list){
+            String isRain = null;
+            if (weather2.isRain()) {
+                isRain = "有雨";
+            }else {
+
+                isRain = "没雨";
+            }
+                System.out.println(simpleDateFormat.format(weather2.getWeek()) + isRain + "   建议穿" + weather2.getClothesType());
+
+
+
+
+        }
     }
 }
